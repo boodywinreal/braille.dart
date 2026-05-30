64f6c72a4e15a0f85ad5d2247e3f405b2e85f5f6
BoodyWin <boodywin.official@gmail.com>
2026-05-30 09:53:18 +0300

    Removed braille_dart.dart (Again)

--- lib/braille_dart.dart
+++
@@ -1,8 +0,0 @@
-/// A helper library for Text-based User Interfaces related to the Braille Unicode symbols.
-/// 
-/// Info that you might need:
-/// - Bit: the integer representation of a dot on the symbol
-// ignore: unnecessary_library_name
-library Braille;
-
-export 'braille.dart';
